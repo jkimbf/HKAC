0ae05e7e60cbb9a8e7a99a7940197e3714d05d6f
Steve Jaehyeok Kim <stevejh.kim0322@gmail.com>
2022-01-28 15:57:46 +0800

    11053 Longest Increasing Sequence

--- 11053_Longest_Increasing_Sequence.py
+++ 11053_Longest_Increasing_Sequence.py
@@ -1,12 +1,17 @@
 from sys import stdin
 
-last_row = [-1, -1]
-for i, num in enumerate(map(int, stdin.read().splitlines()[1].split())):
-    if num > last_row[-1]:
-        last_row.append(num)
-    elif num < last_row[-1] and num > last_row[-2]:
-        last_row[-1] = num
-    elif num < last_row[-2] and num > last_row[-3]:
-        last_row[-2] = num
+nums = list(map(int, stdin.readlines()[1].split()))
+out = [nums[0]]
 
-print(len(last_row)-2)
+for num in nums[1:]:
+    found = False
+    for i, o in enumerate(out):
+        if num <= o:
+            out[i] = num
+            found = True
+            break
+    
+    if not found:
+        out.append(num)
+
+print(len(out))
